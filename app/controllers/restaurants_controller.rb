bd42d003a56a5f6eaadffa962eec612f98c8cf08
sneubig <neubig.stephen@gmail.com>
2014-09-02 17:29:59 -0400

    move logic to model

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,10 +2,5 @@ class RestaurantsController < ApplicationController
 
 	def index
 		@user = User.find(params[:user_id])
-		  if @user.age > 20
-			@restaurants = Restaurant.all
-		  else 
-		  	@restaurants = Restaurant.where(:serves_alcohol => false)
-		  end 
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-  
+  def food_recommendations
+  	Restaurant.all
+  	if self.age > 20
+  		Restaurant.all
+  	else 
+  		Restaurant.where(:serves_alcohol => false)
+  	end
+  end
 end
