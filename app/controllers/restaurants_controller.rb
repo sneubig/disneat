f7a62cd51d9a893c9a838eb30cb3664856fef589
sneubig <neubig.stephen@gmail.com>
2014-09-02 17:14:05 -0400

    filtered restaurants that serve alcohol based on age in controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,11 @@
 class RestaurantsController < ApplicationController
 
 	def index
-		@restaurants = Restaurant.all
 		@user = User.find(params[:user_id])
+		  if @user.age > 20
+			@restaurants = Restaurant.all
+		  else 
+		  	@restaurants = Restaurant.where(:serves_alcohol => false)
+		  end 
 	end
-
 end
